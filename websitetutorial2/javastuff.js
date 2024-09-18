8ffa3472b7573a11f3a6a8496cf59f30c578e9e1
dpang1124 <dpang1124@gmail.com>
2024-09-18 05:02:07 -0400

    added new picture and updated div content

---
+++ websitetutorial2/javastuff.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    const parallax = document.querySelector('.my-picture');
+    const scrollPosition = window.pageYOffset;
+    // Adjust the multiplier (0.5) to control the speed of the scroll effect
+    parallax.style.backgroundPositionY = -scrollPosition * 0.5 + 'px';
+  });
+
+ 
+
+  
