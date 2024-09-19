3bfad0c365b11923c937d48dda5787b20e85967d
dpang1124 <dpang1124@gmail.com>
2024-09-19 06:13:19 -0400

    fixed some bugs and updated education section

--- websitetutorial2/javastuff.js
+++ websitetutorial2/javastuff.js
@@ -1,10 +1,9 @@
 window.addEventListener('scroll', function() {
     const parallax = document.querySelector('.my-picture');
     const scrollPosition = window.pageYOffset;
-    // Adjust the multiplier (0.5) to control the speed of the scroll effect
+    // Adjust the multiplier (0.5) to control the speed of the scroll effect just putting this here for later
     parallax.style.backgroundPositionY = -scrollPosition * 0.5 + 'px';
   });
 
- 
-
+  
   
