859c8ad7f0b66d60fc5ba88e92fdc8d2a2f2fe5e
dpang1124 <dpang1124@gmail.com>
2024-09-24 10:42:48 -0400

    removed idle tutorial

--- websitetutorial1/codtestingidk/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    const background = document.querySelector('.background-image');
-    const scrolled = window.pageYOffset;
-    background.style.transform = 'translateY(' + scrolled * 0.5 + 'px)';
-});
